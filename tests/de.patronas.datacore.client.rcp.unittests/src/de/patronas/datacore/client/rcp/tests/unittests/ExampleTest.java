a99548475346e73b31d6319b790d81c198872874
micha <micha@jackie.local>
2018-02-16 15:34:35 +0100

    Umbennung Test in Unittest

---
+++ tests/de.patronas.datacore.client.rcp.unittests/src/de/patronas/datacore/client/rcp/tests/unittests/ExampleTest.java
@@ -0,0 +1,13 @@
+package de.patronas.datacore.client.rcp.tests.unittests;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ExampleTest {
+	
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
